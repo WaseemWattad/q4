0241dd71c432d7e875eb88ce3895e57c168b5df0
WaseemWattad <104318899+WaseemWattad@users.noreply.github.com>
2022-05-23 23:42:28 +0300

    Add files via upload

---
+++ Sourcee.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define maxlinesize 1023
+
+int main() {
+	/*char string[] = "Sivan|Advanced Topics in C,100|Algorithms,84";
+	char* ptrs[10];
+	strtiktok(string, ptrs);*/
+
+	int number_of_students = 0;
+	int* courses_per_student = NULL;
+
+	char*** bloke = makeStudentArrayFromFile("studentList.txt", &courses_per_student, &number_of_students);
+	for (int i = 0; i < number_of_students; i++) {
+		for (int j = 0; j < courses_per_student[i]; j++) {
+			printf("%s\n", bloke[i][j]);
+		}
+	}
+	return 0;
+}
